170b94bb1466fdd7bc1391807a96f68930df17fe
= <felix.ramirezjr@gmail.com>
2017-05-27 03:50:08 -0700

    Save using create method instead

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,8 @@ class UsersController < ActionController::API
 
   def new
     if( params[:email].present? && params[:password].present? )
-      user = User.new do |u|
-        user.email = params[:email]
-        user.password = params[:password]
-        user.password_confirmation = params[:password_confirmation]
-      end
-      user.save!
+      user = User.create(email: params[:email], password: params[:password],
+                         password_confirmation: params[:password] )
       render json: user
     else
       render json: "false"
