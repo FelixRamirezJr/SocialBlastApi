8861da34e088be91b1da613086c457ec04106725
= <felix.ramirezjr@gmail.com>
2017-05-27 03:44:17 -0700

    Create new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,18 @@ class UsersController < ActionController::API
     render json: User.find(params[:id])
   end
 
+  def new
+    if( params[:email].present? && params[:password].present? )
+      user = User.new do |u|
+        user.email = params[:email]
+        user.password = params[:password]
+        user.password_confirmation = params[:password_confirmation]
+      end
+      user.save!
+      render json: user
+    else
+      render json: "false"
+    end
+  end
+
 end
